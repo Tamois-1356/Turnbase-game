1fd8d1c42e9c3b883d45cd586341a2a9318eda15
Mai Tran Tam <ititwe24068@student.hcmiu.edu.vn>
2025-12-11 10:57:27 +0700

    Delete Skills directory

--- Skills/CooldownManager.java
+++
@@ -1,9 +0,0 @@
-package Skills;
-
-import java.util.List;
-
-public class CooldownManager {
-    public static void tickAll(List<Skill> skills) {
-        for (Skill s : skills) s.tickCooldown();
-    }
-}
