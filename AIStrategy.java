094910323bbb0f7b17828636968766ab1461768f
Huy <88879824+hyhy1@users.noreply.github.com>
2025-12-13 19:59:51 +0700

    Delete AIStrategy.java

--- AIStrategy.java
+++
@@ -1,9 +0,0 @@
-package AI;
-
-import AbstractClass.Unit;
-import TurnManager.ActionExecutor;
-import java.util.List;
-
-public interface AIStrategy {
-    void executeTurn(Unit me, List<Unit> allies, List<Unit> enemies, ActionExecutor executor);
-}
